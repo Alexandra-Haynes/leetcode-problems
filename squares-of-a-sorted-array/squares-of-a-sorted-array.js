d947b7677982f60eb9805ebe5cff61c292c53f29
Alexandra Haynes <113944962+Alexandra-Haynes@users.noreply.github.com>
2023-07-07 16:22:16 -1000

    Time: 104 ms, Memory: 48.7 MB - LeetHub

---
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+    let output=[]
+    let left=0;
+    let right=nums.length-1;
+    for(let i=nums.length-1; i>=0; i--){
+        let square;
+        if(Math.abs(nums[left]) < Math.abs(nums[right])){
+            square=nums[right];
+            right--;
+            
+        } else {
+            square=nums[left]
+            left++
+        }
+        output[i] = square * square;
+    }
+    return output;
+};
