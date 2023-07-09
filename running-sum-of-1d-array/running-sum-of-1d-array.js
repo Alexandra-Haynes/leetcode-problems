8f1c19afd124460e6c8f762ea6559f37cc94c391
Alexandra Haynes <113944962+Alexandra-Haynes@users.noreply.github.com>
2023-07-08 14:40:04 -1000

    Time: 66 ms, Memory: 42.1 MB - LeetHub

---
+++ running-sum-of-1d-array/running-sum-of-1d-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+    //using prefix sum method:
+    let result=[nums[0]]
+    
+    for(let i=1; i<nums.length; i++){
+      result[i] = result[i-1]+nums[i]
+    }
+    return result;
+};
