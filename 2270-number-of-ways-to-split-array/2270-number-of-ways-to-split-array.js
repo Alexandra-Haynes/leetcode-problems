d0abcb566cdb8f4984e4b9309b47205b5886a6d4
Alexandra Haynes <113944962+Alexandra-Haynes@users.noreply.github.com>
2023-07-08 14:25:01 -1000

    Time: 107 ms (48.31%), Space: 56.5 MB (80.21%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.js
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.js
@@ -7,6 +7,7 @@
 
 var waysToSplitArray = function(nums) {
     let ans = 0, leftSection = 0, total = 0;
+    
     for (const num of nums) {
         total += num;
     }
@@ -22,4 +23,4 @@ var waysToSplitArray = function(nums) {
     return ans;
 };
 // time: O(n), we can calculate the sums of the left and right sections in O(1)
-//space: O(1)
+//space: O(1), no prefix array needed
