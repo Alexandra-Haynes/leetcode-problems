5cbd2f722353d980ec84a6fedfce3fa788de88d1
Alexandra Haynes <113944962+Alexandra-Haynes@users.noreply.github.com>
2023-07-10 12:28:14 -1000

    Time: 93 ms (50.45%), Space: 50.7 MB (49.81%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -10,13 +10,12 @@ var subarraySum = function(nums, k) {
   const sumMap = new Map() //the frequency of each prefix sum encountered
   sumMap.set(0, 1) //edge case, empty prefix [] has a sum of 0
 
- 
-
    for (const num of nums) {
         sum += num 
        //because the array can have negative numbers, the same prefix can occur multiple times.
        //we use the hash map to track how many times a prefix has occurred
         count += sumMap.get(sum - k) || 0
+       //frequency of sum - k is equal to the number of subarrays whose sum is equal to k that end at index
         sumMap.set(sum, (sumMap.get(sum) || 0) + 1)
     }
   
