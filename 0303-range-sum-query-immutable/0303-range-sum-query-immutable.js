92200f397e5f02d7bd6eb180465aff4043e6bbdb
Alexandra Haynes <113944962+Alexandra-Haynes@users.noreply.github.com>
2023-07-09 19:25:36 -1000

    Time: 100 ms (85.48%), Space: 49.3 MB (32.22%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.js
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.js
@@ -2,12 +2,13 @@
  * @param {number[]} nums
  */
 var NumArray = function(nums) {
-    let prefix=[0]
     
+    //using the prefix sum method
+    let prefix=[0]
+    //calculate prefix sum
     for(let i=0; i< nums.length; i++){
         prefix[i+1] = prefix[i] + nums[i]
     }
-
 
 /** 
  * @param {number} left 
@@ -24,3 +25,8 @@ NumArray.prototype.sumRange = function(left, right) {
  * var obj = new NumArray(nums)
  * var param_1 = obj.sumRange(left,right)
  */
+
+
+//time: O(n), we iterate over nums just once
+//time complexity for sumRange is O(1)
+//space complexity: O(n), we created an additional array prefix
