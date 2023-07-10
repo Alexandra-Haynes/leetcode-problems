6148cb2549d9ed7458479644e794a6399fde4d99
Alexandra Haynes <113944962+Alexandra-Haynes@users.noreply.github.com>
2023-07-10 11:50:25 -1000

    Time: 72 ms (42.51%), Space: 44.3 MB (48.43%) - LeetHub

---
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var areOccurrencesEqual = function(s) {
+    //approach: because a set ignores duplicates, we can put all the frequencies in a set 
+    //and check if the length is 1 to verify if the frequencies are all the same
+    
+    //creating the hash map for each character
+    let counts = new Map()
+    for (const ch of s) {
+        counts.set(ch, (counts.get(ch) || 0) + 1)
+    }
+    
+    // 
+    
+    let frequencies = new Set()
+    for (const val of counts.values()) {
+        frequencies.add(val)
+    }
+
+    
+    //if all frequencies have the same value => set's size will be 1
+    return frequencies.size == 1
+}
+
+// time: O(n), the cost of populating the map,+ O(n) to convert the map's values to a set, resulting in a linear time complexity
+// space: O(1), because the English alphabet is bounded by a constant 26 letters, 
+               //or O(k), k-the number of characters that could be in the input
