c0eac9c31f5004e4d8b09dea15c710bdb633912a
Alexandra Haynes <113944962+Alexandra-Haynes@users.noreply.github.com>
2023-07-09 19:01:26 -1000

    Time: 65 ms (19.57%), Space: 42.5 MB (7.96%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} gain
+ * @return {number}
+ */
+var largestAltitude = function(gain) {
+    
+    const n=gain.length
+    const prefix = new Array(n+1).fill(0) 
+    let max = 0 //highest altitude
+    
+    for(let i=0; i<n; i++) {
+        //calculate prefix sum
+        prefix[i+1] = prefix[i] + gain[i] 
+        
+        //update max if current is higher
+        if(prefix[i+1] > max) {
+            max=prefix[i+1]
+        }
+    }
+    
+    return max
+}
+
+//time complexity: O(n), we iterated over gain array once
+//space complexity: O(n), we created an additional array, prefix
+
