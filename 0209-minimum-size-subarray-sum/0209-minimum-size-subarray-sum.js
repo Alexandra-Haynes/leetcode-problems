e4790da0b245da9ab7553ccd87980b6be112a773
Alexandra Haynes <113944962+Alexandra-Haynes@users.noreply.github.com>
2023-07-09 14:09:34 -1000

    Time: 75 ms (23.37%), Space: 46.8 MB (9.52%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function(target, nums) {
+    
+  //using the sliding window technique
+  let left = 0
+  let right = 0
+  let currSum = 0
+  let min = Infinity; //any valid subarray length will be smaller and replace the initial value of min
+
+  while (right < nums.length) {
+    currSum += nums[right]
+    right++
+
+    while (currSum >= target) {
+      min = Math.min(min, right - left)
+      currSum -= nums[left]
+      left++
+    }
+  }
+
+  return min === Infinity ? 0 : min
+};
