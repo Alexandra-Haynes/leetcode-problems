842281638a71a17e76388edf3256e5aacebf3a3d
Alexandra Haynes <113944962+Alexandra-Haynes@users.noreply.github.com>
2023-07-09 14:33:49 -1000

    Time: 109 ms (26.66%), Space: 54.9 MB (51.77%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
@@ -4,27 +4,23 @@
  * @return {number}
  */
 var findMaxAverage = function(nums, k) {
-    //using sliding window technique:
-    let left=0;
-    let right=k;
-    let average=0;
-    let sum=0;
-    let max=0;
+    let left=0, right=k, sum=0, average=0, max=0;
+    
     
     for(let i=0; i<k; i++){
         sum+=nums[i]
     }
+    average=sum/k;
+    max=average;
+    
+    
+    while(right < nums.length){
+        sum+=nums[right];
+        sum-=nums[left];
         average=sum/k;
-        max=average
-        
-        while(right < nums.length){
-            sum+=nums[right];
-            sum-=nums[left]
-            average=sum/k;
-            max=Math.max(max,average);
-            left++;
-            right++;
-        }
-       
+        max=Math.max(max, average)
+        left++;
+        right++;
+ }
     return max
 };
