f11bb64f34e69f357c6ef7fb05c307491e0efd86
Alexandra Haynes <113944962+Alexandra-Haynes@users.noreply.github.com>
2023-07-10 10:54:30 -1000

    Time: 69 ms (60.92%), Space: 48.2 MB (8.39%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    
+  const n = nums.length;
+  const numSet = new Set(nums)
+  //we are using a Set because of its efficient lookup, O(1) time complexity
+  for (let i = 0; i <= n; i++) {
+    if (!numSet.has(i)) {
+      return i
+    }
+  }
+
+  return -1 // default return value, if no missing number is found 
+}
