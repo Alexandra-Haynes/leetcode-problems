b9210bbc3f1a2888fe233c4c9d7dbd4d4fbf98f3
Alexandra Haynes <113944962+Alexandra-Haynes@users.noreply.github.com>
2023-07-10 19:08:26 -1000

    Time: 60 ms (71.22%), Space: 44.2 MB (70.24%) - LeetHub

---
+++ 1133-largest-unique-number/1133-largest-unique-number.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var largestUniqueNumber = function(nums) {
+    const frequency = new Map()
+    
+     //count the frequency of each integer
+  for (const num of nums) {
+    frequency.set(num, (frequency.get(num) || 0) + 1)
+  }
+    
+    //initialize max with -1
+    let largestUnique = -1
+    
+     //find the largest integer that occurs only once
+  for (const [num, count] of frequency) {
+    if (count === 1 && num > largestUnique) {
+      largestUnique = num
+    }
+  }
+
+  return largestUnique 
+    
+}
