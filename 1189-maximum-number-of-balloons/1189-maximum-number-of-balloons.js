224fb0e5d20ceffc92aa336603ddc319336443a1
Alexandra Haynes <113944962+Alexandra-Haynes@users.noreply.github.com>
2023-07-11 09:25:43 -1000

    Time: 70 ms (50.92%), Space: 44 MB (39.15%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} text
+ * @return {number}
+ */
+var maxNumberOfBalloons = function(text) {
+    const charCount = new Map()
+
+  //count the frequency of each character in the given text
+  for (const char of text) {
+    charCount.set(char, (charCount.get(char) || 0) + 1)
+  }
+
+  //count the required characters for "balloon"
+  const balloonChars = new Map([
+    ["b", 1],
+    ["a", 1],
+    ["l", 2],
+    ["o", 2],
+    ["n", 1]
+  ])
+
+  let maxInstances = Infinity //any subsequent value we find will be smaller than Infinity
+
+  //find the maximum count of instances
+  for (const [char, count] of balloonChars) {
+      
+    if (!charCount.has(char)) {
+      return 0 //if any 'balloon' ch is missing, return 0 instances
+    }
+      
+    const instances = Math.floor(charCount.get(char) / count)
+    //checks if the word has every ch in 'balloon' at least once
+    maxInstances = Math.min(maxInstances, instances) //update the max, if necessary
+  }
+
+  return maxInstances
+}
+
+//time: O(n), iterated through the text string once, iterating through "balloon" takes constant time 
+//space:O(1), limited to the number of unique characters in the text string and the fixed number of characters for "balloon"
