9455105fcbbe4d909169a1d49fd288916b376541
Alexandra Haynes <113944962+Alexandra-Haynes@users.noreply.github.com>
2023-07-10 16:00:18 -1000

    Time: 398 ms (84.88%), Space: 108.2 MB (83.72%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.js
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.js
@@ -3,26 +3,29 @@
  * @return {number[][]}
  */
 var findWinners = function(matches) {
-    const seen = new Set();
-    const lossesCount = {};
+  const seen = new Set();
+  const lossesCount = new Map();
 
-    for (const [winner, loser] of matches) {
-      seen.add(winner);
-      seen.add(loser);
-      lossesCount[loser] = (lossesCount[loser] || 0) + 1;
+  for (const [winner, loser] of matches) {
+    seen.add(winner);
+    seen.add(loser);
+    lossesCount.set(loser, (lossesCount.get(loser) || 0) + 1);
+  }
+
+  const zeroLose = [];
+  const oneLose = [];
+
+  for (const player of seen) {
+    const count = lossesCount.get(player) || 0;
+    if (count === 0) {
+      zeroLose.push(player);
+    } else if (count === 1) {
+      oneLose.push(player);
     }
+  }
 
-    const zeroLose = [];
-    const oneLose = [];
+  zeroLose.sort((a, b) => a - b);
+  oneLose.sort((a, b) => a - b);
 
-    for (const player of seen) {
-      const count = lossesCount[player] || 0;
-      if (count === 0) {
-        zeroLose.push(player);
-      } else if (count === 1) {
-        oneLose.push(player);
-      }
-    }
-
-    return [zeroLose.sort((a, b) => a - b), oneLose.sort((a, b) => a - b)];
+  return [zeroLose, oneLose];
   }
