98ecdbfe13eeb88bb62e8e1be1f983d8fa49cddc
Alexandra <113944962+Alexandra-Haynes@users.noreply.github.com>
2023-07-12 10:50:40 -1000

    Time: 73 ms (6.58%), Space: 44.4 MB (7.48%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.js
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.js
@@ -4,7 +4,7 @@
  */
 var sumOfUnique = function(nums) {
     
-    
+    // keeping track of the frequency of each num
     let numCount=new Map()
     let sum=0
     
@@ -13,8 +13,12 @@ var sumOfUnique = function(nums) {
     }
     
     for(const [num, count] of numCount) {
+        //if num appears only once => it's unique => add it to sum
         if(count===1) sum+=num
     }
     
     return sum
 };
+
+//time complexity: O(n), we iterate through the array twice: once to count the occurrences and once to calculate the sum of unique elements
+//space complexity: O(n), numCount map stores the count of each number. In the worst case, all elements in nums are distinct and the map would store all the numbers.
