5346ddc080c53793cf4f1b18d607c97a8d5b1faa
Alexandra <113944962+Alexandra-Haynes@users.noreply.github.com>
2023-07-12 10:19:59 -1000

    Time: 64 ms (36.43%), Space: 42.6 MB (29.29%) - LeetHub

--- 1496-path-crossing/1496-path-crossing.js
+++ 1496-path-crossing/1496-path-crossing.js
@@ -4,19 +4,24 @@
  */
 var isPathCrossing = function(path) {
     
-    
+    //use a set to keep track of each unique point to arrive at
     let visited = new Set([0,0])
     
     visited.add("0,0")
     
+    //we start at (0,0)
     let y=0
     let x=0
+    
+    
+    //for each move to add or substract an unit from x or y
     for(let coord of path){
         if (coord === 'N') y++
         if (coord === 'S') y--
         if (coord === 'E') x++
         if (coord === 'W') x--
         
+    //check if we've been in that point before    
         let newPoint = `${x},${y}`
         if(visited.has(newPoint) ) return true
         visited.add(newPoint)
@@ -24,3 +29,6 @@ var isPathCrossing = function(path) {
     
     return false
 };
+
+//time: O(n), we iterate through the path once
+//space: O(n), in the worst case, if the path doesn't cross itself, we store all visited positions in the visited set.
