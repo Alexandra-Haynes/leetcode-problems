dffdb5f5e6517e05329412176cea3de2a187d043
Alexandra <113944962+Alexandra-Haynes@users.noreply.github.com>
2023-07-11 20:42:24 -1000

    Time: 80 ms (81.14%), Space: 44.6 MB (95.89%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,19 +3,18 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-  //sliding window approach to keep track of the current substring without repeating characters
+  //sliding window = current substring without repeating characters
     
     let maxLength=0
     let start=0 //the start index of the current substring
-//hash map to store the most recent index of each ch seen
     
-    const chMap=new Map()
+    const chMap=new Map() //hash map to store the most recent index of each ch seen
     
 //we iterate through the string from left to right
     for (let end = 0; end < s.length; end++) {
         const ch = s[end]
-        if(chMap.has(ch) && chMap.get(ch)>=start)
-            
+        if(chMap.has(ch) && chMap.get(ch) >= start)
+    //explanation: if the character is a repeat and its index >= start index => the repeat character falls within the current substring and needs to be excluded from the next substring
     //if ch is in the substring, update the pointer
             start = chMap.get(ch) + 1 //to maintain a string without repeating ch
         
