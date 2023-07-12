511a85296771481cd1edefbf905de6f4de102fd0
Alexandra <113944962+Alexandra-Haynes@users.noreply.github.com>
2023-07-12 11:09:12 -1000

    Time: 67 ms (57.42%), Space: 44.5 MB (32.54%) - LeetHub

--- 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.js
+++ 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.js
@@ -4,24 +4,24 @@
  */
 var findLucky = function(arr) {
     
-    let max=-1
-    
-    let arrMap=new Map()
-    
-    for(const num of arr) {
-        
-        arrMap.set(num, (arrMap.get(num) || 0) +1)
+  const freqMap = new Map() //tracks the frequency of each num
+
+  //count the frequency of each number
+  for (const num of arr) {
+    freqMap.set(num, (freqMap.get(num) || 0) + 1)
+  }
+
+  let largestLucky = -1
+
+  //find the largest lucky integer
+  for (const [num, freq] of freqMap) {
+    if (num === freq && num > largestLucky) {
+      largestLucky = num
     }
-    
-    for(let i=0; i<arr.length; i++) {
-        
-        let num = arr[i]
-        
-        if(arrMap.get(num) === num) {
-            max=Math.max(max, num)
-        }
-        
-    }
-    
-    return max
+  }
+
+  return largestLucky
 };
+
+//time complexity: O(n), iterates through the array twice: once to count the frequencies and once to find the largest lucky integer
+//space complexity: O(n), the worst case: all elements in arr are distinct => map stores all the numbers
