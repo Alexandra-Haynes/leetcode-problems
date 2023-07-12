8d998732d6de89276be513e55dfd288ffb8df191
Alexandra <113944962+Alexandra-Haynes@users.noreply.github.com>
2023-07-11 19:28:41 -1000

    Time: 84 ms (71.76%), Space: 44 MB (91.60%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function(ransomNote, magazine) {
+    
+    let ransomMap= new Map()
+    let magazineMap = new Map()
+    
+    //populate magazine map with each ch and its frequency
+    
+    for(let i=0; i<magazine.length;i++){
+        const ch = magazine[i]   
+        magazineMap.set(ch, (magazineMap.get(ch) || 0) + 1)
+    }
+    
+    //check if ransomNote can be build
+    
+    for(let i=0; i< ransomNote.length; i++) {
+        const ch = ransomNote[i]
+        //if the ch doesn't exist in magazineMap or the count is 0 => return false
+        if(!magazineMap.has(ch) || magazineMap.get(ch) === 0) return false
+    
+        //update the count of ch  in ransomMap
+        
+        ransomMap.set(ch, (ransomMap.get(ch) || 0) + 1)
+                      
+      //in the same time, decrease the count in the magazineMap
+        magazineMap.set(ch, magazineMap.get(ch)  - 1)
+    }
+    
+    //  if we got this far it means that all characters in ransomNote can be constructed from magazine
+ 
+    return true
+};
