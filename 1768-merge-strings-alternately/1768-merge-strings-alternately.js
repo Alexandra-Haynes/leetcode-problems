61a1e276a8a331b6293512905351f4aa9e88ff16
Alexandra <113944962+Alexandra-Haynes@users.noreply.github.com>
2023-08-30 19:42:23 -1000

    Time: 48 ms (86.56%), Space: 42.3 MB (44.55%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function(word1, word2) {
+    
+    let merged = '';
+  let i = 0, j = 0;
+
+  while (i < word1.length && j < word2.length) {
+    merged += word1[i];
+    i++;
+
+
+    merged += word2[j];
+    j++;
+  }
+
+ 
+  while (i < word1.length) {
+    merged += word1[i];
+    i++;
+  }
+
+  while (j < word2.length) {
+    merged += word2[j];
+    j++;
+  }
+
+  return merged;
+    
+};
