72d015fd92869602ef8d761f985d41cb576aaee8
Alex <113944962+Alexandra-Haynes@users.noreply.github.com>
2023-09-19 15:57:39 -1000

    Time: 553 ms (14.89%), Space: 126.3 MB (47.73%) - LeetHub

---
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteMiddle = function(head) {
+    
+    //using the fast and slow pointer technique
+    
+    if (!head || !head.next) return null //one or zero nodes
+    
+    let slow=head
+    let fast=head
+    let prev=null
+    
+    while(fast && fast.next){
+        prev=slow
+        slow=slow.next
+        fast=fast.next.next
+    }
+    
+     // remove the slow pointer (middle node):
+    if (prev) {
+        prev.next = slow.next;
+    }
+    
+    return head
+}
+
+//time complexity: O(n), we interate through the linked list once with the pointers
+//space complexity: O(1), the space used by the slow, fast, and prev pointers is constant and does not grow with the size of the input linked list
