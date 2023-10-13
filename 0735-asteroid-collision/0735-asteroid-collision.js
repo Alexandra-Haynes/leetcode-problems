96c239691b3e144c9dae382c712addb1b3f0ef88
Alex <113944962+Alexandra-Haynes@users.noreply.github.com>
2023-10-12 15:30:26 -1000

    Time: 57 ms (79.31%), Space: 44.5 MB (13.52%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.js
+++ 0735-asteroid-collision/0735-asteroid-collision.js
@@ -3,41 +3,35 @@
  * @return {number[]}
  */
 var asteroidCollision = function(asteroids) {
-  const stack = [];
-
+  const stack = []
   for (const asteroid of asteroids) {
-    handleAsteroid(asteroid);
+    handleAsteroid(asteroid)
   }
-
   function handleAsteroid(asteroid) {
     if (stack.length === 0 || asteroid > 0) {
-      stack.push(asteroid);
+      stack.push(asteroid)
     } else {
       while (stack.length > 0) {
-        const top = stack.pop();
+        const top = stack.pop()
         if (top < 0) {
-          // If the top asteroid is also moving left (negative), add the current asteroid to the stack.
-          stack.push(top);
-          stack.push(asteroid);
-          break;
+          stack.push(top)
+          stack.push(asteroid)
+          break
         } else {
           const collisionResult = top + asteroid;
           if (collisionResult === 0) {
-            // Both asteroids explode.
-            break;
+            break
           } else if (collisionResult > 0) {
-            // Top asteroid survives, push it back and break.
-            stack.push(top);
-            break;
+            stack.push(top)
+            break
           } else if (stack.length === 0) {
-            // If the stack is empty, add the current asteroid to the stack.
-            stack.push(asteroid);
-            break;
+            stack.push(asteroid)
+            break
           }
         }
       }
     }
   }
 
-  return stack;
+  return stack
 };
